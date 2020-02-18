bfad9b6d3911a05750f21413d505523d178bd36f
Daniel Bachler <daniel@danielbachler.de>
2020-02-18 11:04:43 +0100

    Add JSME code, dummy invocation. Does not work yet

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
+import './JSME_2017-02-26/jsme/jsme.nocache'
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import styles from './styles.css'
 
-import styles from './styles.css'
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
+}
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class Jsme extends React.Component {
+  constructor(props) {
+    super()
+    console.log("test")
+    this.myRef = React.createRef()
+    if (props.hasOwnProperty("id")) {
+      this.id = props.id
+    }
+    else {
+      this.id = "jsme" + getRandomInt(1, 100000)
+    }
+  }
+  componentDidMount() {
+    jsmeApplet = new JSApplet.JSME(this.id, "380px", "340px");
+  }
+
+  componentWillUnmount() {
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
+    return <div ref={this.myRef} id={this.id}>
+      <p>{this.id}</p>
       </div>
-    )
   }
 }
